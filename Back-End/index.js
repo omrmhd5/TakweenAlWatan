2e44479cbf9815b3d8b519c642a76b5f3c0c05a4
omrmhd5 <omrmhd54@gmail.com>
2025-07-09 22:59:54 +0300

    Cors Trying to Fix

--- Back-End/index.js
+++ Back-End/index.js
@@ -8,14 +8,16 @@ const PestControlReportRoutes = require("./Routers/pestControlReportRoutes");
 const app = express();
 const PORT = process.env.PORT;
 
-const corsOptions = {
-  origin: "https://takween-al-watan.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: ,
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
+app.use("/", cors({ origin: "https://takween-al-watan.vercel.app" }));
 
 app.use(express.json());
 app.use("/api/auth", UserRoutes);
