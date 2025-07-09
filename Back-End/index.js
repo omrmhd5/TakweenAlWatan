4542e3ffb801df93b5401c31bd6d983d98a4cfe7
omrmhd5 <omrmhd54@gmail.com>
2025-07-09 22:27:18 +0300

    CORS Fix

--- Back-End/index.js
+++ Back-End/index.js
@@ -8,7 +8,14 @@ const PestControlReportRoutes = require("./Routers/pestControlReportRoutes");
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://takween-al-watan.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", UserRoutes);
 app.use("/api/reports", PestControlReportRoutes);
