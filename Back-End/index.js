9dd03fc5cfab58ceb07596acf5848701391b25bd
omrmhd5 <omrmhd54@gmail.com>
2026-04-05 09:47:20 +0200

    Remove Dockerfile and fly.toml configuration files

--- Back-End/index.js
+++ Back-End/index.js
@@ -6,7 +6,7 @@ const UserRoutes = require("./Routers/UserRoutes");
 const PestControlReportRoutes = require("./Routers/pestControlReportRoutes");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: ["http://localhost:5173", "https://takween-al-watan.vercel.app"],
